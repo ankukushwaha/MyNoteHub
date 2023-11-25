693ab72fdbeff576aa3439fe8656c799b8954ee1
ankukushwaha <ankurkushwaha7408@gmail.com>
2023-11-25 23:45:00 +0530

    authenticate with jsonwebtoken and cookie

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,19 +1,25 @@
 import User from "../models/Users.js"
 import bcrypt from "bcrypt";
 import { Router } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
+dotenv.config();
 const router = Router();
 
 router.post("/", (req,res) => {
-    const {username, email, password} = req.body;
-    User.findOne({email: email, username: username}).then((foundUser) => {
+    const {email, password} = req.body;
+    User.findOne({email: email}).then((foundUser) => {
         if(foundUser){
             bcrypt.compare(password, foundUser.password, function(err, result) {
                 if (result === true) {
-                    res.status(201).json("login successfully");
+                    const token = jwt.sign({ id : foundUser._id }, process.env.JWT_SECRET);
+                    // hiding password to sent with other informations 
+                    const {password: pass, ...rest} = foundUser._doc;
+                    res.cookie('access token', token, { httpOnly: true }).status(200).json(rest);
                 }
                 else{
-                    res.status(500).json({error: "please enter correct password"}); 
+                    res.status(500).json({error: "please enter correct credentials"}); 
                 }
             });
         }
